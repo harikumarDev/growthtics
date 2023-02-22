a05209f90f75d60199673d7cab9cac4b05c14b5b
Hari kumar <hari70319@gmail.com>
2023-02-22 13:37:27 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("feedback-form");
+
+const submitFeedback = (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const feedback = document.getElementById("feedback").value;
+
+  fetch("https://chat-api.growthtics.com/feedback", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, feedback }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert("Feedback submitted successfully!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+form.addEventListener("submit", submitFeedback);
